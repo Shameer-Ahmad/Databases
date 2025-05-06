d4a6033374c987bd02fdbfe83379e37bd675eb1f
Shameer-Ahmad <ahmadsk@bc.edu>
2025-05-06 00:00:43 -0400

    Synced changes with maddie's db.py modification

--- P3/website/db.py
+++ P3/website/db.py
@@ -1,4 +1,7 @@
 import sqlite3
 
+
 def get_db_connection():
-    return sqlite3.connect("foodies.sqlite3")
+    conn = sqlite3.connect("foodies.sqlite3")
+    conn.row_factory = sqlite3.Row
+    return conn
