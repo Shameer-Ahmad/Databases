d5799b943f66e8807223849363544eac4a22b344
Simmonmr <97983777+Simmonmr@users.noreply.github.com>
2025-05-05 21:14:45 -0400

    Update db.py

--- P3/website/db.py
+++ P3/website/db.py
@@ -1,4 +1,6 @@
 import sqlite3
 
 def get_db_connection():
-    return sqlite3.connect("foodies.sqlite3")
+    conn = sqlite3.connect("foodies.sqlite3")
+    conn.row_factory = sqlite3.Row
+    return conn
