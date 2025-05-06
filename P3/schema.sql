7775098f9d7d4832b64c31607a3524b0f598a9ac
Simmonmr <97983777+Simmonmr@users.noreply.github.com>
2025-05-05 21:15:09 -0400

    Update schema.sql
    Updated schema.sql in line with P2 for restaurants implementation

--- P3/schema.sql
+++ P3/schema.sql
@@ -7,3 +7,28 @@ CREATE TABLE user (
     email TEXT NOT NULL UNIQUE,
     password TEXT NOT NULL
 );
+
+DROP TABLE IF EXISTS restaurant;
+
+CREATE TABLE restaurant (
+    restaurant_id INTEGER PRIMARY KEY,
+    restaurant_name TEXT NOT NULL,
+    street_number TEXT NOT NULL,
+    street_name TEXT NOT NULL,
+    apt_number TEXT,
+    city TEXT NOT NULL,
+    state TEXT NOT NULL,
+    zip_code TEXT NOT NULL,
+    cuisine_type TEXT,
+    FOREIGN KEY (zip_code) REFERENCES neighborhood_zip(zip_code)
+);
+
+DROP TABLE IF EXISTS restaurant_phone;
+
+CREATE TABLE restaurant_phone (
+    restaurant_id INTEGER,
+    phone_number TEXT,
+    PRIMARY KEY (restaurant_id, phone_number),
+    FOREIGN KEY (restaurant_id) REFERENCES restaurant(restaurant_id),
+    CHECK (phone_number LIKE '___-___-____')
+);
